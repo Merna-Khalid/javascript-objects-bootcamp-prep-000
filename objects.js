ee5ff253cbe54b2eb47646e586b369177b726314
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-15 11:12:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-playlist
+var playlist = {};
+
+function updatePlaylist(obj, artist, song) {
+  obj[artist] = song
+  return obj
+}
+
+function removeFromPlaylist(obj, artist) {
+  delete obj.artist
+  return obj
+}
